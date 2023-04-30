8ae39de609ce9289746e48eb9c898bb68eb883b4
Pranav Waghmare <piyushw55@gmail.com>
2023-04-30 17:13:19 +0530

    [Pranav]Added: Ability to create polymorphism Method overriding example

---
+++ Oops_Concept_Examples/PolymorphismMethodOverride.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+
+
+namespace Oops_Concept_Examples
+{
+    class TestOverride
+    {
+        public class Employee
+        {
+            public string Name { get; }
+
+            protected decimal _basepay;
+
+           
+            public Employee(string name, decimal basepay)
+            {
+                Name = name;
+                _basepay = basepay;
+            }
+
+            public virtual decimal CalculatePay()
+            {
+                return _basepay;
+            }
+        }
+
+        public class SalesEmployee : Employee
+        {
+          
+            private decimal _salesbonus;
+
+         
+            public SalesEmployee(string name, decimal basepay, decimal salesbonus) : base(name, basepay)
+            {
+               
+                _salesbonus = salesbonus;
+            }
+
+       
+            public override decimal CalculatePay()
+            {
+                return _basepay + _salesbonus;
+            }
+        }
+
+       
+    }
+}
